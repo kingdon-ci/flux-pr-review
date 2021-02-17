e16c57c19819f45eda585cc38d3b1ce4f14afb32
Kingdon Barrett <kingdon@weave.works>
2021-02-17 17:44:05 -0500

    copy zero-indexed CSV input into Google Sheet

--- Gemfile
+++ Gemfile
@@ -4,4 +4,5 @@ ruby '2.7.2'
 
 gem "export-pull-requests", "~> 0.3.3"
 gem "google_drive"
-gem "pry"
+gem "pry-rails"
+gem "rails", "~> 6.1"
--- lib/put_in_spreadsheet.rb
+++ lib/put_in_spreadsheet.rb
@@ -1,18 +1,22 @@
 require 'forwardable'
+require 'csv'
 
 module PutIn
   class Spreadsheet
     extend Forwardable
-    def_delegators :@properties, :[]
+    def_delegators :@properties, :[], :[]=
 
-    def initialize(config_json: "config.json", spreadsheet_id:)
+    def initialize(config_json: "config.json", spreadsheet_id:, input_file: 'pr.csv')
       @spreadsheet_id = spreadsheet_id
       session = GoogleDrive::Session.from_config(config_json)
 
       @properties = {
         session: session,
-        ws: session.spreadsheet_by_key(spreadsheet_id).worksheets[0]
+        ws: session.spreadsheet_by_key(spreadsheet_id).worksheets[0],
+        input_filename: input_file
       }
+
+      self[:pr_csv] = CSV.read(input_filename)
 
       check_for_safety!
       call
@@ -22,8 +26,29 @@ module PutIn
       self[:ws]
     end
 
+    def input_csv
+      self[:pr_csv]
+    end
+
+    def input_filename
+      self[:input_filename]
+    end
+
     def call
-      binding.pry
+      num_rows, num_cols = input_csv.length, input_csv[0].length
+
+      (1..num_rows).each do |row|
+        (1..num_cols).each do |col|
+          # input_csv is zero-indexed arrays so, ...
+          input_value = input_csv&.[](row-1)&.[](col-1)
+
+          if input_value.present?
+            ws[row, col] = input_value
+          end
+        end
+      end
+
+      ws.save
     end
 
     def check_for_safety!
