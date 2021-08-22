cd7d4a6eebfcc6b149610c46b912bffb28874450
Kingdon Barrett <kingdon@weave.works>
2021-08-22 18:12:17 -0400

    add some locale settings

--- jenkins/rake-ci.sh
+++ jenkins/rake-ci.sh
@@ -4,5 +4,10 @@ source /etc/profile.d/rvm.sh
 rvm ${RUBY}@testing
 
 bundle check
+
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 bundle exec ruby graphql_microtest.rb && \
 	bundle exec rspec
