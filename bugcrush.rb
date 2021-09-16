d3bb3c37fb44adc638531997127aad397515bea6
Kingdon Barrett <kingdon@weave.works>
2021-09-16 12:28:06 -0400

    bug scrub 0010

--- bugcrush.rb
+++ bugcrush.rb
@@ -29,9 +29,9 @@ config =
     }
   elsif ARGV[1] == "v2"
     {
-      google_sheet_id:   "1eEY9yzdwEP1YVn-z5rC0OxVZR0PqTTsI45xbNuf9bXg",
-      scrub_event_id:    "0009",
-      previous_event_id: "0008",
+      google_sheet_id:   "1twt-sgIFTcMTVzZKWwXeiTrtEGCcKKiiGSev5BzDCjg",
+      scrub_event_id:    "0010",
+      previous_event_id: "0009",
       csvinput_filename: ARGV[0],
       discussion_csvinput_filename: ARGV[2],
     }
