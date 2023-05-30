25d41e90da40c285cae9ece3622eaa5836ba0918
Kingdon Barrett <kingdon@weave.works>
2023-05-30 17:51:31 -0400

    Flux Bug Scrub 0071
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- params.rb
+++ params.rb
@@ -1,7 +1,7 @@
 module Params
-  @@google_sheet_id   = "1v6HrPBq16guDHmThBFp9cUfYvTYUql8GmbjJqbmwYD0"
-  @@scrub_event_id    = "0070"
-  @@previous_event_id = "0069"
+  @@google_sheet_id   = "1Ll4kQeVmQfnpbGN9TteBGzGupO_tMr_D2Q4CRK4Abxg"
+  @@scrub_event_id    = "0071"
+  @@previous_event_id = "0070"
 
   def google_sheet_id
     @@google_sheet_id
