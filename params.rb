3c085fc58c880a830c7a4a3f1bc5eeb09f539af5
Kingdon Barrett <kingdon@weave.works>
2023-08-31 12:52:29 -0400

    Bug Scrub 0077
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- params.rb
+++ params.rb
@@ -1,7 +1,7 @@
 module Params
-  @@google_sheet_id   = "1c3jhcfEvVK8bI1URjwHTjZVSuByBuOS9OS1S_RvM-V0"
-  @@scrub_event_id    = "0076"
-  @@previous_event_id = "0075"
+  @@google_sheet_id   = "1GLb74u25SofsdWv1DYqSmbrE0BN4UZI94z3AzbkLNpQ"
+  @@scrub_event_id    = "0077"
+  @@previous_event_id = "0076"
 
   def google_sheet_id
     @@google_sheet_id
